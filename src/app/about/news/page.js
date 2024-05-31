0982e7d41d96207a71a356be9a03e453e6c84257
jadycain <jadycain@gmail.com>
2024-05-31 21:53:26 +0800

    feat: 移除框線

--- src/app/about/news/page.js
+++ src/app/about/news/page.js
@@ -31,8 +31,8 @@ export default function LatestPage() {
         "「TCCF PITCHING 提案大會」總投資金額上看新台幣一億 全球徵件六月起跑催生影視佳作",
       time: "2024/05/10",
       linkText: "大會公告",
-      link: "/",
-      image: "/images/news/news_1.jpg",
+      link: "about/news/20240510",
+      image: "/images/about/news/20240510/banner.jpg",
       tag: "大會新聞",
       type: "news",
     },
@@ -41,8 +41,8 @@ export default function LatestPage() {
       title: "2024 TCCF 創意內容大會日期與活動單元公佈",
       time: "2024/05/02",
       linkText: "大會公告",
-      link: "/",
-      image: "/images/news/news_2.png",
+      link: "about/news/20240502",
+      image: "/images/about/announcement/20240502/banner.jpg",
       tag: "大會公告",
       type: "announcement",
     },
@@ -52,8 +52,8 @@ export default function LatestPage() {
         "「2023 TCCF 創意內容大會」圓滿成功！ 法日韓美星等 29 國專業人士齊聚臺北預估延伸交易金額超越去年成績",
       time: "2023/11/15",
       linkText: "大會公告",
-      link: "/",
-      image: "/images/news/news_3.png",
+      link: "about/news/20231115",
+      image: "/images/about/news/20231115/banner.jpg",
       tag: "大會新聞",
       type: "news",
     },
@@ -63,8 +63,8 @@ export default function LatestPage() {
         "2023 TCCF 提案大會《雪水消融的季節》、《Mother Maybe》勇奪 3 萬美金雙首獎",
       time: "2023/11/10",
       linkText: "大會公告",
-      link: "/",
-      image: "/images/news/news_4.jpeg",
+      link: "about/news/20231110",
+      image: "/images/about/news/20231110/banner.jpeg",
       tag: "大會新聞",
       type: "news",
     },
@@ -89,8 +89,8 @@ export default function LatestPage() {
                 className={styles.latest__tabList_tabItem}
                 onClick={() => handleTabClick(tab.type)}
                 style={{
-                  background: activeTab === tab.type ? "#171717" : "#fafafa",
-                  color: activeTab === tab.type ? "#fafafa" : "#171717",
+                  background: activeTab === tab.type ? "#171717" : "white",
+                  color: activeTab === tab.type ? "white" : "#171717",
                 }}
               >
                 {tab.name}
